3beb6b8cdd6f141e33845808e222247ae8e2df84
Carolina Campos <carolinacampos.dev@gmail.com>
2024-03-22 12:09:05 -0300

    Adicionando listas suspensas

--- cadastrar.php
+++ cadastrar.php
@@ -163,27 +163,103 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <input type="hidden" name="matricula_original" value="<?php echo $registro['matricula_servidor']; ?>">
             <?php endif; ?>
 
-            Nome: <input type="text" class="input-cadastro" name="nome" value="<?php echo $registro['nome'] ?? ''; ?>" required><br>
+            Nome: <input type="text" class="input-cadastro" name="nome" value="<?php echo $registro['nome'] ?? ''; ?>" required>
             Matrícula do Servidor:
-            <input type="text" name="matricula_servidor" oninput="validarMatricula(this)" pattern="[0-9]{7}" title="Digite exatamente 7 números" value="<?php echo $registro['matricula_servidor'] ?? ''; ?>" required><br>
-            Unidade de Lotação: <input type="text" input-cadastro name="unidade_lotacao" value="<?php echo $registro['unidade_lotacao'] ?? ''; ?>" required><br>
-            Categoria Funcional: <input type="text" input-cadastro name="categoria_funcional" value="<?php echo $registro['categoria_funcional'] ?? ''; ?>" required><br>
-            Central: <input type="text" name="central" value="<?php echo $registro['central'] ?? ''; ?>"><br>
-            Gestor: <input type="radio" name="gestor" value="1" <?php if (isset($registro['gestor']) && $registro['gestor'] == '1') echo 'checked'; ?> required>Sim
-            <input type="radio" name="gestor" value="0" <?php if (isset($registro['gestor']) && $registro['gestor'] == '0') echo 'checked'; ?> required>Não<br>
-            Motivo da Informação: <textarea name="motivo_informacao" required><?php echo $registro['motivo_informacao'] ?? ''; ?></textarea><br>
-            Períodos de férias: <input type="text" input-cadastro name="qtd_periodos_ferias" value="<?php echo $registro['qtd_periodos_ferias'] ?? ''; ?>" required><br>
+            <input type="text" name="matricula_servidor" oninput="validarMatricula(this)" pattern="[0-9]{7}" title="Digite exatamente 7 números" value="<?php echo $registro['matricula_servidor'] ?? ''; ?>" required>
+            Unidade de Lotação:
+            <select name="unidade_lotacao" required>
+                <option value="">Selecione...</option>
+                <option value="11025050 - APS RIACHUELO" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025050 - APS RIACHUELO') echo 'selected'; ?>>APS RIACHUELO</option>
+                <option value="115251 - SERVIÇO DE GERENCIAMENTO DE BENEFÍCIOS" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '115251 - SERVIÇO DE GERENCIAMENTO DE BENEFÍCIOS') echo 'selected'; ?>>SERVIÇO DE GERENCIAMENTO DE BENEFÍCIOS</option>
+                <option value="1152522 - SAREC" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '1152522 - SAREC') echo 'selected'; ?>>SAREC</option>
+                <option value="11025110 - APS SÃO JOÃO NEPOMUCENO" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025110 - APS SÃO JOÃO NEPOMUCENO') echo 'selected'; ?>>APS SÃO JOÃO NEPOMUCENO</option>
+                <option value="11025040 - APS SÃO DIMAS" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025040 - APS SÃO DIMAS') echo 'selected'; ?>>APS SÃO DIMAS</option>
+                <option value="11025060 - APS LEOPOLDINA" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025060 - APS LEOPOLDINA') echo 'selected'; ?>>APS LEOPOLDINA</option>
+                <option value="11025080 - APS ALÉM PARAÍBA" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025080 - APS ALÉM PARAÍBA') echo 'selected'; ?>>APS ALÉM PARAÍBA</option>
+                <option value="11025070 - APS MURIAÉ" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025070 - APS MURIAÉ') echo 'selected'; ?>>APS MURIAÉ</option>
+                <option value="11025010 - APS CARANGOLA" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025010 - APS CARANGOLA') echo 'selected'; ?>>APS CARANGOLA</option>
+                <option value="11025090 - APS PALMA" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025090 - APS PALMA') echo 'selected'; ?>>APS PALMA</option>
+                <option value="11025020 - CATAGUASES" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025020 - CATAGUASES') echo 'selected'; ?>>CATAGUASES</option>
+                <option value="11025 - GERÊNCIA EXECUTIVA" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '11025 - GERÊNCIA EXECUTIVA') echo 'selected'; ?>>GERÊNCIA EXECUTIVA</option>
+                <option value="115252 - SGREC" <?php if (isset($registro['unidade_lotacao']) && $registro['unidade_lotacao'] == '115252 - SGREC') echo 'selected'; ?>>SGREC</option>
+            </select>
+            <label for="categoria_funcional">Categoria Funcional:</label>
+            <select name="categoria_funcional" id="categoria_funcional" required>
+                <option value="">Selecione...</option>
+                <option value="Analista" <?php if (isset($registro['categoria_funcional']) && $registro['categoria_funcional'] == "Analista") echo "selected"; ?>>Analista</option>
+                <option value="Analista Assistência Social" <?php if (isset($registro['categoria_funcional']) && $registro['categoria_funcional'] == "Analista Assistência Social") echo "selected"; ?>>Analista Assistência Social</option>
+                <option value="Analista Terapeuta Ocupacional" <?php if (isset($registro['categoria_funcional']) && $registro['categoria_funcional'] == "Analista Terapeuta Ocupacional") echo "selected"; ?>>Analista Terapeuta Ocupacional</option>
+                <option value="Técnico" <?php if (isset($registro['categoria_funcional']) && $registro['categoria_funcional'] == "Técnico") echo "selected"; ?>>Técnico</option>
+            </select>
+            <label for="central">Central:</label>
+            <select name="central" id="central" required>
+                <option value="">Selecione...</option>
+                <option value="MAN" <?php if (isset($registro['central']) && $registro['central'] == "MAN") echo "selected"; ?>>MAN</option>
+                <option value="MAN(BI)" <?php if (isset($registro['central']) && $registro['central'] == "MAN(BI)") echo "selected"; ?>>MAN(BI)</option>
+                <option value="RD" <?php if (isset($registro['central']) && $registro['central'] == "RD") echo "selected"; ?>>RD</option>
+                <option value="GEX" <?php if (isset($registro['central']) && $registro['central'] == "GEX") echo "selected"; ?>>GEX</option>
+                <option value="DJ" <?php if (isset($registro['central']) && $registro['central'] == "DJ") echo "selected"; ?>>DJ</option>
+                <option value="DIRBEN" <?php if (isset($registro['central']) && $registro['central'] == "DIRBEN") echo "selected"; ?>>DIRBEN</option>
+                <option value="CES" <?php if (isset($registro['central']) && $registro['central'] == "CES") echo "selected"; ?>>CES</option>
+            </select>
+            Gestor:
+            <select name="gestor" required>
+                <option value="">Selecione...</option>
+                <option value="1" <?php if (isset($registro['gestor']) && $registro['gestor'] == '1') echo 'selected'; ?>>Sim</option>
+                <option value="0" <?php if (isset($registro['gestor']) && $registro['gestor'] == '0') echo 'selected'; ?>>Não</option>
+            </select>
+            Observação: <textarea name="motivo_informacao" required><?php echo $registro['motivo_informacao'] ?? ''; ?></textarea>
+            <label for="qtd_periodos_ferias">Período de Férias:</label>
+            <select name="qtd_periodos_ferias" id="qtd_periodos_ferias" required>
+                <option value="" <?php if (!isset($registro['qtd_periodos_ferias']) || $registro['qtd_periodos_ferias'] == '') echo 'selected'; ?>>Selecione</option>
+                <option value="1" <?php if (isset($registro['qtd_periodos_ferias']) && $registro['qtd_periodos_ferias'] == '1') echo 'selected'; ?>>Período único</option>
+                <option value="2" <?php if (isset($registro['qtd_periodos_ferias']) && $registro['qtd_periodos_ferias'] == '2') echo 'selected'; ?>>Dois períodos</option>
+                <option value="3" <?php if (isset($registro['qtd_periodos_ferias']) && $registro['qtd_periodos_ferias'] == '3') echo 'selected'; ?>>Três períodos</option>
+            </select>
+            <br>
 
-            <!-- Campos de data para os períodos -->
-            <?php for ($i = 1; $i <= 3; $i++) : ?>
-                <label for="data_inicio_<?php echo $i; ?>">Data Início Período <?php echo $i; ?></label>
-                <input type="date" name="data_inicio_<?php echo $i; ?>" id="data_inicio_<?php echo $i; ?>" value="<?php echo $registro ? date('Y-m-d', strtotime($registro['data_inicio_' . $i])) : ''; ?>">
+            <div id="periodos_ferias">
+                <!-- Campos de data para o primeiro período -->
+                <label for="data_inicio_1" style="display: none;">Data Início Período 1:</label>
+                <input type="date" name="data_inicio_1" id="data_inicio_1" style="display: none;" value="<?php echo $registro ? date('Y-m-d', strtotime($registro['data_inicio_1'])) : ''; ?>">
+                <label for="data_fim_1" style="display: none;">Data Fim Período 1:</label>
+                <input type="date" name="data_fim_1" id="data_fim_1" style="display: none;" value="<?php echo $registro ? date('Y-m-d', strtotime($registro['data_fim_1'])) : ''; ?>">
 
-                <label for="data_fim_<?php echo $i; ?>">Data Fim Período <?php echo $i; ?></label>
-                <input type="date" name="data_fim_<?php echo $i; ?>" id="data_fim_<?php echo $i; ?>" value="<?php echo $registro ? date('Y-m-d', strtotime($registro['data_fim_' . $i])) : ''; ?>">
-            <?php endfor; ?>
+                <!-- Campos de data para o segundo período -->
+                <label for="data_inicio_2" style="display: none;">Data Início Período 2:</label>
+                <input type="date" name="data_inicio_2" id="data_inicio_2" style="display: none;" value="<?php echo $registro ? date('Y-m-d', strtotime($registro['data_inicio_2'])) : ''; ?>">
+                <label for="data_fim_2" style="display: none;">Data Fim Período 2:</label>
+                <input type="date" name="data_fim_2" id="data_fim_2" style="display: none;" value="<?php echo $registro ? date('Y-m-d', strtotime($registro['data_fim_2'])) : ''; ?>">
 
+                <!-- Campos de data para o terceiro período -->
+                <label for="data_inicio_3" style="display: none;">Data Início Período 3:</label>
+                <input type="date" name="data_inicio_3" id="data_inicio_3" style="display: none;" value="<?php echo $registro ? date('Y-m-d', strtotime($registro['data_inicio_3'])) : ''; ?>">
+                <label for="data_fim_3" style="display: none;">Data Fim Período 3:</label>
+                <input type="date" name="data_fim_3" id="data_fim_3" style="display: none;" value="<?php echo $registro ? date('Y-m-d', strtotime($registro['data_fim_3'])) : ''; ?>">
+            </div>
 
+            <script>
+                document.getElementById('qtd_periodos_ferias').addEventListener('change', function() {
+                    var qtdPeriodos = this.value;
+                    var periodosDiv = document.getElementById('periodos_ferias');
+
+                    // Esconder todos os campos de data
+                    for (var i = 1; i <= 3; i++) {
+                        document.querySelector('label[for="data_inicio_' + i + '"]').style.display = 'none';
+                        document.getElementById('data_inicio_' + i).style.display = 'none';
+                        document.querySelector('label[for="data_fim_' + i + '"]').style.display = 'none';
+                        document.getElementById('data_fim_' + i).style.display = 'none';
+                    }
+
+                    // Exibir os campos de data relevantes com base na seleção do usuário
+                    for (var i = 1; i <= qtdPeriodos; i++) {
+                        document.querySelector('label[for="data_inicio_' + i + '"]').style.display = 'block';
+                        document.getElementById('data_inicio_' + i).style.display = 'block';
+                        document.querySelector('label[for="data_fim_' + i + '"]').style.display = 'block';
+                        document.getElementById('data_fim_' + i).style.display = 'block';
+                    }
+                });
+            </script>
             <input type="submit" value="<?php echo $id ? 'Atualizar' : 'Cadastrar'; ?>">
             <?php if (!empty($validationErrors)) : ?>
                 <ul class="error-list">
